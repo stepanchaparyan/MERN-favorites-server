41051f8ee9aef9bb6fe813a3762d0e0f41e02a66
stepanchaparyan <stepanchaparyan@gmail.com>
2021-04-05 23:08:35 +0400

    Update swagger.yaml

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/api/favItem', auth, require('./routes/favItem'));
 app.use('/api/profile', auth, require('./routes/profile'));
 app.use('/api/upload', require('./routes/upload'));
 app.use('/api/books', auth, require('./routes/books'));
-app.use('/api/products', require('./routes/products'));
+app.use('/api/products', auth, require('./routes/products'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
